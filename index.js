12f99e48262b7de55dbcbeb1bcf2b105dca47dca
Miranda Wilson <miranda@snowplowanalytics.com>
2022-08-05 09:21:00 +0100

    Maybe add a comment

---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const github = require("@actions/github");
+
+const issueBranchRegex = /^issue\/(\d+)/;
+
+async function run() {
+  const octokit = github.getOctokit(process.env.ACCESS_TOKEN);
+
+  const context = github.context;
+  // const owner = context.repo.owner;
+  // const repo = context.repo.repo;
+
+  // ref for pull looks like "refs/pull/19/merge"
+  if (!context.ref.split("/")[1] === "pull") {
+    console.log("This isn't a pull request.");
+    return;
+  }
+
+  // const pullNumber = context.ref.split("/")[2];
+
+  try {
+    const { data: pull } = await octokit.rest.pulls.get({
+      owner: owner,
+      repo: repo,
+      pull_number: pullNumber,
+    });
+    const branchName = pull.head.ref;
+    console.log(branchName);
+
+    let issueNumber;
+    try {
+      issueNumber = branchName.match(issueBranchRegex)[1];
+    } catch {
+      console.log(`Couldn't find an issue number in "${branchName}"`);
+      return;
+    }
+
+    addLabelToIssue(octokit, owner, repo, issueNumber, "status:has_pr");
+  } catch {
+    console.log(`Failed to find PR ${pullNumber}`);
+  }
+
+  const owner = "mscwilson";
+  const repo = "try-out-actions-here";
+  const pullNumber = 88;
+  addCommentWithIssueNumber(octokit, owner, repo, pullNumber, issueNumber);
+}
+
+run();
+
+async function addCommentWithIssueNumber(
+  octokit,
+  owner,
+  repo,
+  pullNumber,
+  issueNumber
+) {
+  try {
+    octokit.rest.issues.createComment({
+      owner: owner,
+      repo: repo,
+      issue_number: pullNumber,
+      body: "a comment",
+    });
+  } catch (error) {
+    console.log(`Failed to add a comment to PR/issue ${issueNumber}`);
+    console.log(error);
+  }
+}
